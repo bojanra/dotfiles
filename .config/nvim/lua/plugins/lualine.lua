80520e59d39a1d6d52f294d682413e0a1b995b42
Bojan Ramšak <bojan.ramsak@rtvslo.si>
2025-04-25 21:57:18 +0200

    Some unsuccesfull attempts to run Minuet-ai on ollama with gemma3

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,13 +1,13 @@
-if true then
-  return {}
-end
+--if true then return {} end
 
 return {
-  "nvim-lualine/lualine.nvim",
-  optional = true,
-  event = "VeryLazy",
-  opts = function(_, opts)
-    local icon = LazyVim.config.icons.kinds.TabNine
-    table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("cmp_tabnine", icon))
-  end,
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    -- opts = function(_, opts)
+    --   local icon = LazyVim.config.icons.kinds.TabNine
+    --   table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("cmp_tabnine", icon))
+    -- end,
+  },
 }
